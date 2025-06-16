4377fe5efc2f608cf6637fcea8e953e42c16608f
irodriguezyanine <ignaciorodriguezyanine@gmail.com>
2025-06-16 10:36:31 -0400

    Create BarberProfilePage.jsx

---
+++ frontend/src/pages/BarberProfilePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BarberProfile from '../components/barbers/BarberProfile';
+
+const BarberProfilePage = () => {
+  return <BarberProfile />;
+};
+
+export default BarberProfilePage;
