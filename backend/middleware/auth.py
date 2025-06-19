9772ec19ef764c86486c626db02d09ffb2ad97e2
irodriguezyanine <ignaciorodriguezyanine@gmail.com>
2025-06-19 01:03:04 -0400

    Update auth.py

--- backend/middleware/auth.py
+++ backend/middleware/auth.py
@@ -1,14 +1,8 @@
-# cortate/backend/middleware/auth.py
+from flask import jsonify
 
-from functools import wraps
-from flask import request, jsonify
-
-# Simulación de autenticación simple
-def require_auth(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        token = request.headers.get("Authorization")
-        if not token or token != "Bearer supertoken":
-            return jsonify({"error": "No autorizado"}), 401
-        return f(*args, **kwargs)
-    return decorated_function
+def handle_error(e):
+    response = {
+        "error": str(e),
+        "mensaje": "Ha ocurrido un error inesperado."
+    }
+    return jsonify(response), 500
