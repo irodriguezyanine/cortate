eb4509c0c3ac2cf4722c9bc44f936d237484e423
irodriguezyanine <ignaciorodriguezyanine@gmail.com>
2025-06-25 20:46:25 -0400

    Delete backend/config/database.js

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-# cortate/backend/config/database.py
-
-# Simulación de base de datos en memoria (puede migrarse a MongoDB o SQLite luego)
-usuarios = []
-peluqueros = []
-reservas = []
-reseñas = []
-penalizaciones = []
