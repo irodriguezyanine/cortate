7e27f601a8696a1fe8e06ca8bac2d63928d6e72c
irodriguezyanine <ignaciorodriguezyanine@gmail.com>
2025-06-25 20:46:36 -0400

    Delete backend/config/config.js

--- backend/config/config.js
+++
@@ -1,8 +0,0 @@
-# cortate/backend/config/config.py
-
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
-    UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER") or "uploads"
-    MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 10MB
